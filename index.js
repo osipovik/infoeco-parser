4d7ca4df68dced89a82ebb0a38525870fd79dabf
Ivan Osipov <osipovik89@gmail.com>
2016-08-30 00:03:31 +0300

    добил парсинг списка стационарных пунктов. рефакторинг

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
+// Подключаем необходимые модули
 var request = require("request");
 var cheerio = require("cheerio");
 var tress = require("tress");
 var scorocode = require("scorocode");
+// костанты с ключами для работы с BAAS Scorocode
+const APP_ID = "88e98d83c5f4edc68589184843ad6904";
+const JS_KEY = "2213dc3c13272159af8345764cfd55d2";
+const MASTER_KEY = "91d8cf4388e9c32390b49b07aed16e74";
+const PARSE_URL = "http://ecomobile.infoeco.ru";
 
-// //Создаем очередь с задержкой выполнения 1 секунда
+var mapContent;
+
+// Создаем очередь с задержкой выполнения 1 секунда
 var queue = tress(function(job, done) {
 	parseMainPointInfo(job);
 	// Функция callback
@@ -22,61 +30,94 @@ queue.success = function(data) {
     // console.log('Job ' + this + ' successfully finished. Result is ' + data);
 }
 
-//Получаем страницу с табличеым представлением графика стоянок
-request("http://ecomobile.infoeco.ru/grafik-stoyanok.html", function(error, response, body) {
-	if (error) {
-		console.log("error: " + error);
-	} else {
-		// Чистим старые данные
-		removeOldData();
-
-		var $ = cheerio.load(body);
-		$("table.table tr:not(:first-child)").each(function() {
-			queue.push($(this));
-			// return false;
-		});
-	}
-});
-
-//Получаем страницу с табличеым представлением графика стоянок
-// request("http://ecomobile.infoeco.ru/staczionarnyie-punktyi.html", function(error, response, body) {
+// Получаем страницу с табличеым представлением графика стоянок
+// request(PARSE_URL + "/grafik-stoyanok.html", function(error, response, body) {
 // 	if (error) {
 // 		console.log("error: " + error);
 // 	} else {
+// 		// Чистим старые данные
+// 		removeOldData();
+
 // 		var $ = cheerio.load(body);
-// 		$("div#content dl dt").each(function(index) {
-// 			console.log(index + " - " + $(this).text());
-
-// 			var statPointInfo = $("div#content dl dd").eq(index).text().split("\r\n");
-
-// 			statPointInfo.forEach(function(item, i, arr) {
-// 				statPointInfo[i] = item.replace(/^\s+|\s+$/g,'');
-// 			});
-
-// 			var pointInfo = {};
-
-// 			pointInfo.district = $(this).text().split(" ")[0];
-// 			pointInfo.address = statPointInfo[1];
-// 			pointInfo.phone = statPointInfo[2];
-
-// 			var arTime = statPointInfo[3].match(/([0-9\.]*)\s\S{2}\s([0-9\.]*)/);
-
-// 			pointInfo.time
-
-// 			console.log(pointInfo);
-// 			console.log(arTime);
-// 			// trim string
-// 			// string.replace(/^\s+|\s+$/g,''); 
-
-// 			return false;
+// 		$("table.table tr:not(:first-child)").each(function() {
+// 			queue.push($(this));
+// 			// return false;
 // 		});
 // 	}
 // });
 
+request(PARSE_URL + "/34.html", function(error, response, body) {
+	if (error) {
+		console.error("error: " + error);
+	} else {
+		$ = cheerio.load(body);
+		mapContent = $("script").text();
+		getStatMapPointInfo();
+	}
+});
+
+function getStatMapPointInfo() {
+	// Получаем страницу с табличеым представлением стационарных пунктов сбора
+	request(PARSE_URL + "/staczionarnyie-punktyi.html", function(error, response, body) {
+		if (error) {
+			console.error("error: " + error);
+		} else {
+			var $ = cheerio.load(body);
+			$("div#content dl dt").each(function(index) { 
+				var ddItem = $("div#content dl dd").eq(index);
+				var arStatPointInfo = ddItem.text().split("\r\n");
+
+				arStatPointInfo.forEach(function(item, i, arr) {
+					arStatPointInfo[i] = item.replace(/^\s+|\s+$/g,'');
+				});
+
+				var pointInfo = {};
+
+				pointInfo.district = $(this).text().split(" ")[0];
+				pointInfo.address = arStatPointInfo[1];
+				pointInfo.phone = arStatPointInfo[2].replace(/[^0-9\+-]+/g, '');
+
+				var arTime = arStatPointInfo[3].match(/([0-9\.]*)\s\S{2}\s([0-9\.]*)/);
+
+				pointInfo.time_start = timeToSeconds(arTime[1]);
+				pointInfo.time_end = timeToSeconds(arTime[2]);
+
+				var statPointInfoHtml = ddItem.html();
+				var result = statPointInfoHtml.match(/<a\shref=".+statPointId=([0-9]+)"/);
+				var mapPointId = result[1];
+
+
+				var pattern = "marks\\['stat'\\]\\[" + mapPointId + "\\]\\s=\\snew\\symaps\\.Placemark\\(\\[([0-9\\.]*,[0-9\\.]*)\\],";
+				var result = mapContent.match(pattern);
+
+				if (result) {
+					pointInfo.coord = result[1].split(",");
+				}
+
+				// Ищем фотограцию места, если она есть в парметре balloonContent
+				pattern = "marks\\['stat'\\]\\[" + mapPointId + "\\]\\s=\\snew\\symaps\\.Placemark[\\s\\S]*balloonContent:\\s'<p><a\\shref=\"(.*)\"\\starget";
+				// pattern = /accentMark=\snew\symaps\.Placemark[\s\S]*balloonContent:\s'<p><a\shref="(.*)"\starget/;
+				result = mapContent.match(pattern);
+
+				if (result) {
+					pointInfo.photo = PARSE_URL + "/" + result[1];
+				}
+				console.log(pointInfo);
+
+				// return false;
+			});
+		}
+	});
+}
+
+function timeToSeconds(time) {
+	var arTime = time.split(".");
+	return (((parseInt(arTime[0]) * 60) + parseInt(arTime[1])) * 60);
+}
+
 function parseMainPointInfo(pointInfoLine) {
 	var mapPointId = 0;
 	var point = {};
-
 	var tdList = pointInfoLine.find("td");
 
 	point.district = tdList.eq(0).text();
@@ -86,8 +127,8 @@ function parseMainPointInfo(pointInfoLine) {
 	var timeStartEnd = tdList.eq(3).text().split("-");
 
 	if (timeStartEnd.length == 2) {
-		point.time_start = timeStartEnd[0];
-		point.time_end = timeStartEnd[1];
+		point.time_start = timeToSeconds(timeStartEnd[0]);
+		point.time_end = timeToSeconds(timeStartEnd[1]);
 	}
 
 	var link = pointInfoLine.find("td:last-child a").attr("href");
@@ -102,13 +143,13 @@ function parseMainPointInfo(pointInfoLine) {
 }
 
 function parseMapPointInfo(mapPointId, point) {
-	var url = "http://ecomobile.infoeco.ru/34.html&datePointId=" + mapPointId;
+	var url = PARSE_URL + "/34.html&datePointId=" + mapPointId;
 	var coord = [];
 	var info = point;
 
 	request(url, function(error, response, body) {
 		if (error) {
-			console.log("error: " + error);
+			console.error("error: " + error);
 		} else {
 			var $ = cheerio.load(body);
 
@@ -128,7 +169,7 @@ function parseMapPointInfo(mapPointId, point) {
 			result = script.match(pattern);
 
 			if (result) {
-				info.photo = "http://ecomobile.infoeco.ru/" + result[1];
+				info.photo = PARSE_URL + "/" + result[1];
 			}
 		}
 
@@ -138,23 +179,16 @@ function parseMapPointInfo(mapPointId, point) {
 
 function prepareDataForDB(pointInfo) {
 	var arDate = pointInfo.date.split(".");
-	pointInfo.date = new Date(arDate[2], arDate[1]-1, arDate[0]);
-	// console.info(date);
-
-	var arTimeStart = pointInfo.time_start.split(".");
-	pointInfo.time_start = ((arTimeStart[0] * 60) + arTimeStart[1]) * 60;
-	// console.info(timeStart);
-
-	var arTimeEnd = pointInfo.time_end.split(".");
-	pointInfo.time_end = ((arTimeEnd[0] * 60) + arTimeEnd[1]) * 60;
-	// console.info(timeEnd);
+	pointInfo.date = new Date(arDate[2], arDate[1]-1, arDate[0], 9);
+	// console.info(arDate);
 
 	var queryItem = new scorocode.Query("points");
 
-	console.info(pointInfo);
+	// console.info(pointInfo);
 
 	queryItem
 		.equalTo("address", pointInfo.address)
+		.equalTo("date", pointInfo.date)
 		.equalTo("time_start", pointInfo.time_start)
 		.equalTo("time_end", pointInfo.time_end)
 		.find().then((result) => {
@@ -162,7 +196,7 @@ function prepareDataForDB(pointInfo) {
 				addNewPointToDB(pointInfo);
 			}
 		}).catch((error) => {
-	    	console.log("Что-то пошло не так: \n", error)
+	    	console.error("Что-то пошло не так: \n", error)
 		});
 
 	// console.info(pointInfo);
@@ -170,9 +204,9 @@ function prepareDataForDB(pointInfo) {
 
 function removeOldData() {
 	scorocode.Init({
-		ApplicationID: "88e98d83c5f4edc68589184843ad6904",
-    	JavaScriptKey: "2213dc3c13272159af8345764cfd55d2",
-    	MasterKey: "91d8cf4388e9c32390b49b07aed16e74"
+		ApplicationID: APP_ID,
+    	JavaScriptKey: JS_KEY,
+    	MasterKey: MASTER_KEY
 	});
 
 	var queryItems = new scorocode.Query("points");
@@ -184,14 +218,14 @@ function removeOldData() {
 			// console.info(finded);
 			queryItems.remove(finded)
 				.then((removed) => {
-					console.info(removed);
+					console.log(removed);
 				})
 				.catch((error) => {
-					console.info("Что-то пошло не так: \n", error);
+					console.error("Что-то пошло не так: \n", error);
 				});
 		})
 		.catch((error) => {
-            console.log("Что-то пошло не так: \n", error)
+            console.error("Что-то пошло не так: \n", error)
         });
 }
 
@@ -216,8 +250,8 @@ function addNewPointToDB(pointInfo) {
 	// Сохраняем объект
 	pointItem.save()
 		.then((saved) => {
-			console.info("successfully saved");
+			console.log("successfully saved");
 		}).catch((error) => {
-			console.info("facking fail: \n", error);
+			console.error("facking fail: \n", error);
 		});
 }
